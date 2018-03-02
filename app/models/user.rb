2c3a1b7878f1f0dc1dfc19a96d4910e1cf455dc3
Raghu Betina <raghu@firstdraft.com>
2018-03-02 15:52:55 +0000

    Direct association between users and goal_comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :goal_comments,
+             :dependent => :destroy
+
   has_many   :coachings,
              :dependent => :destroy
 
